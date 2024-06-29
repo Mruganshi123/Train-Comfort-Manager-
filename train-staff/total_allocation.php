017efadda28edd59026ee91a0feea1f830e788a2
Mruganshi <mruganshic6@gmail.com>
2024-06-29 19:30:26 +0530

    dashboard

--- train-staff/total_allocation.php
+++ train-staff/total_allocation.php
@@ -25,11 +25,11 @@ include '../database/conn.php';
       <div class="container-fluid">
         <div class="row mt-9 mb-3 pl-5 pr-5">
           <div class="col-md-4 offset-md-2">
-
+            
           </div>
           <div class="col-md-4">
             <div class="card bg-success">
-
+              
             </div>
           </div>
         </div>
@@ -46,8 +46,6 @@ include '../database/conn.php';
                         <th rowspan="2">Name</th>
                         <th rowspan="2">Phone Number</th>
                         <th rowspan="2">Train Number</th>
-                        <th rowspan="2">Comaprtment No</th>
-                        <th rowspan="2">Seat no</th>
                         <th rowspan="2">Bed Roll</th>
                         <th rowspan="2">Reserve Date</th>
                       </tr>
@@ -56,53 +54,49 @@ include '../database/conn.php';
                     <tbody>
                       <?php
                       $qry = "
-                     SELECT 
-                       reservations.reserv_id AS reservation_id,
-                       reservations.comartment_no AS compt_no,
-                       reservations.seat_no AS seat_no,
-                       reservations.bed_roll_reserved AS bed_reserved,
-                       users.name AS user_name,
-                       users.phone_number AS user_phone_number,
-                       reservations.reservation_date AS reserve_date,
-                       trains.train_number AS train_number
-                       
-                     FROM 
-                       reservations
-                     JOIN 
-                       users ON reservations.user_id = users.user_id
-                     JOIN 
-                       trips ON reservations.trip_id = trips.trip_id
-                     JOIN 
-                       trains ON trips.train_id = trains.train_id;
-      ";
+                      SELECT 
+                        reservations.reserv_id AS reservation_id,
+                        reservations.bed_roll_reserved AS bed_reserved,
+                        users.name AS user_name,
+                        users.phone_number AS user_phone_number,
+                        reservations.reservation_date AS reserve_date,
+                        trains.train_number AS train_number
+                      FROM 
+                        reservations
+                      JOIN 
+                        users ON reservations.user_id = users.user_id
+                      JOIN 
+                        trips ON reservations.trip_id = trips.trip_id
+                      JOIN 
+                        trains ON trips.train_id = trains.train_id;
+                      ";
 
                       $res = mysqli_query($conn, $qry);
                       if (!$res) {
-                        echo "Error occurred: " . mysqli_error($conn);
+                          echo "Error occurred: " . mysqli_error($conn);
                       } else {
-                        while ($row = mysqli_fetch_assoc($res)) {
+                          while ($row = mysqli_fetch_assoc($res)) {
                       ?>
                           <tr>
                             <td><?php echo $row['reservation_id']; ?></td>
                             <td><?php echo $row['user_name']; ?></td>
                             <td><?php echo $row['user_phone_number']; ?></td>
                             <td><?php echo $row['train_number']; ?></td>
-                            <td><?php echo $row['compt_no']; ?></td>
-                            <td><?php echo $row['seat_no']; ?></td>
                             <td>
-                              <?php
-                              if ($row['bed_reserved'] == 0) {
-                                echo '<span class="badge badge-warning">Not Reserved</span>';
-                              } else {
-                                echo '<span class="badge badge-success">Reserved</span>';
-                              }
-
-                              ?></td>
+                                <?php 
+                                if($row['bed_reserved'] == 0) {
+                                    echo '<span class="badge badge-warning">Not Reserved</span>';
+                                }
+                                else{
+                                    echo '<span class="badge badge-success">Reserved</span>';        
+                                }
+                                
+                                ?></td>
                             <td><?php echo $row['reserve_date']; ?></td>
                           </tr>
-                      <?php
-                        }
-                      }
+                      <?php 
+                          }
+                      } 
                       ?>
                     </tbody>
                   </table>
