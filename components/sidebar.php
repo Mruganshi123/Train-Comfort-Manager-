c255845be4422df17d8dcdc05c966ea068d2aa75
Mruganshi <mruganshic6@gmail.com>
2024-06-29 13:15:45 +0530

    train-staff

---
+++ components/sidebar.php
@@ -0,0 +1,158 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="../asset/dist/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">IRCTC</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../asset/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <!-- <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div> -->
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+               
+              </p>
+            </a>
+            
+          </li>
+
+
+          
+          
+          <li class="nav-item">
+            <a href="../train-staff/passanger.php" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+               total allocation
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">6</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../asset/pages/layout/top-nav.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>already </p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../asset/layout/top-nav-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>onboard</p>
+                </a>
+              </li>
+              
+              
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="../asset/pages/calendar.html" class="nav-link">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>
+                Requests
+                <span class="badge badge-info right">2</span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                Inventory
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../asset/pages/charts/chartjs.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>View</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../asset/pages/charts/flot.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Update</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" id="logoutLink" class="nav-link">
+            <i class="nav-icon fas fa-chart-pie"></i>
+              <p>
+                log out
+
+              </p>
+            </a>
+
+          </li>
+          
+          <!-- <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Tables
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="../asset/pages/tables/simple.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Simple Tables</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../asset/pages/tables/data.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>DataTables</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="../asset/pages/tables/jsgrid.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>jsGrid</p>
+                </a>
+              </li>
+            </ul>
+          </li> -->
+          
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
---
+++ train-staff/passanger.php
@@ -0,0 +1,105 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>measurment table</title>
+  <style>
+    th {
+      text-align: center;
+      /* Center-align all table headers */
+    }
+  </style>
+  <?php
+  include("../components/header.php");
+  ?>
+  <!-- Other script tags and your custom JavaScript code -->
+
+</head>
+<!-- ... (head section and other HTML structure) ... -->
+
+<body class="hold-transition sidebar-mini">
+  <!-- ... (wrapper and other HTML structure) ... -->
+  <div class="wrapper">
+    <?php
+    include("C:/xampp/htdocs/ws/kruti_admin/admin/include/sidebar.php");
+    ?>
+
+    <div class="content-wrapper">
+
+      <!-- Table to display measurement data -->
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                    <tr>
+                      <!-- Table headers -->
+                      <th rowspan="2">User ID</th>
+                      <?php
+                      $dates = [];
+                      // Collect unique dates for each user
+                      foreach ($data as $user) {
+                        $dates = array_merge($dates, array_keys($user));
+                      }
+                      $dates = array_unique($dates);
+                      $dates = array_values(array_diff($dates, ['User ID']));
+
+                      // Display date headers with height and weight
+                      foreach ($dates as $date) {
+                        echo "<th colspan='2'>$date</th>";
+                      }
+                      ?>
+                    </tr>
+                    <tr>
+                      <!-- Sub-headers for height and weight -->
+                      <?php
+                      foreach ($dates as $date) {
+                        echo "</th><th>Height</th><th>Weight</th>";
+                      }
+                      ?>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <!-- Display data rows -->
+                    <?php
+                    if (!empty($data)) {
+                      foreach ($data as $user_id => $user_data) {
+                        echo "<tr>";
+                        echo "<td>$user_id</td>";
+                        foreach ($dates as $date) {
+                          if (isset($user_data[$date])) {
+                            echo "<td>" . ($user_data[$date]['height'] !== null ? $user_data[$date]['height'] : '-') . "</td>";
+                            echo "<td>" . ($user_data[$date]['weight'] !== null ? $user_data[$date]['weight'] : '-') . "</td>";
+                          } else {
+                            echo "<td>-</td><td>-</td>";
+                          }
+                        }
+                        echo "</tr>";
+                      }
+                    } else {
+                      echo "<tr><td colspan='" . (count($dates) * 2 + 1) . "'>No data found</td></tr>";
+                    }
+                    ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+  include("../components/footer.php");
+  ?>
+
+  <!-- ... (footer and other HTML structure) ... -->
+</body>
+
+</html>
